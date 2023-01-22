a37bc456edcc462e035f6080b72cae7459dc3482
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-01-22 16:51:30 +0100

    logic updated

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,10 +4,15 @@ let startButton = document.querySelector("#start");
 let questionCtnEl = document.querySelector("#questions-ctn")
 let questionEl = document.querySelector("#question-title")
 let answerButton = document.querySelector("#answer-buttons")
+let nextButton = document.querySelector("#next");
+
 
 startButton.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', function () {
+    nextQuestion();
+})
 
-shuffleQuestion = questionsArray.sort( function() { 
+let shuffleQuestion = questionsArray.sort( function() { 
     return Math.random() - 0.5; 
 });
 
@@ -30,18 +35,28 @@ function renderQuestion(que) {
         btn.addEventListener("click", selectAnswer);
         answerButton.appendChild(btn);
     });
+}
 
+function reset() {
+    nextButton.classList.add("hide");
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
 }
 
 function nextQuestion() {
-    reset()
+    reset();
     renderQuestion(shuffleQuestion[0]);
 }
 
 function selectAnswer(event) {
-
+    let selectedButton = event.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct)
+    Array.from(answerButton.children).forEach(function (btn) {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove("hide");
 }
 
-function reset() {
 
-}
