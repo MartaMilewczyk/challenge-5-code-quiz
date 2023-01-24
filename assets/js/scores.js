a7758e9bd985ea9991824fbff5a486eb6b09e0e0
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-01-24 12:10:15 +0100

    view highscores update

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,11 +2,9 @@ let highScoresEl = document.querySelector("#highscores");
 let clearButton = document.querySelector("#clear");
 const highScores = JSON.parse(localStorage.getItem("username")) || [];
 
-// highScoresEl.innerHTML = `Player: ${player.initials} - Score: ${player.scores}`
-
-    // highScoresEl.innerHTML = `
-    // ${highScores.map((player) =>
-    // `<li>User:${player.initials} - Score:${player.scores}</li>`).join("")}`;
+    highScoresEl.innerHTML = `
+    ${highScores.map((player) =>
+    `<li>User: ${player.initials} - Score: ${player.scores}</li>`).join("")}`;
 
 clearButton.addEventListener("click", clearStorage);
 
